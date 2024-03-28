13f213981224d73590d804d62d7e1d8bd90a3297
varshneytanisha <117394394+varshneytanisha@users.noreply.github.com>
2024-03-28 18:14:51 +0530

    add files

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from '../Signup'
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Signup />
+
+    </div>
+   
+    
+  )
+}
+
+export default App
